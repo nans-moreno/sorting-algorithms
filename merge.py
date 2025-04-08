397120286eea4b1a0e36334b7b7c6c1775682c53
nans-moreno <nans.moreno@laplateforme.io>
2025-04-08 10:14:43 +0200

    merge + sort

---
+++ merge.py
@@ -0,0 +1,40 @@
+def merge_sort(my_list):
+    """
+    Trie une liste en utilisant l'algorithme de tri fusion.
+
+    Args:
+        my_list: La liste à trier.
+
+    Returns:
+        La liste triée.
+    """
+    if len(my_list) > 1:
+        mid = len(my_list) // 2
+        left_half = my_list[:mid]
+        right_half = my_list[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                my_list[k] = left_half[i]
+                i += 1
+            else:
+                my_list[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            my_list[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            my_list[k] = right_half[j]
+            j += 1
+            k += 1
+    return my_list
